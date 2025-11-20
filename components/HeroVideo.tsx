7cd5a64388a9ec06aaa3cb5b7eff6ef339ec08f6
mininormi <csmxlm@gmail.com>
2025-11-21 03:45:15 +0800

    auto-update-周五-11-21_03-45

--- components/HeroVideo.tsx
+++ components/HeroVideo.tsx
@@ -1,25 +1,24 @@
 // components/HeroVideo.tsx
 export default function HeroVideo() {
-    return (
-      <div
-        className="
-          fixed top-0 left-0 
-          w-screen h-screen 
-          -z-10 overflow-hidden
-        "
-      >
-        {/* Vimeo 全屏背景视频 */}
-        <iframe
-          src="https://player.vimeo.com/video/1115222113?dnt=1&app_id=122963&autoplay=1&muted=1&loop=1&background=1"
-          className="absolute top-0 left-0 w-full h-full object-cover"
-          allow="autoplay; fullscreen; picture-in-picture"
-          frameBorder="0"
-          style={{ pointerEvents: "none" }}
-        />
-  
-        {/* 黑色透明遮罩，让文字可读 */}
-        <div className="absolute inset-0 bg-black/40" />
-      </div>
-    );
-  }
-  
+  return (
+    <div
+      className="
+        fixed inset-0 
+        -z10
+        overflow-hidden
+        bg-black
+        pointer-events-none
+      "
+    >
+      <iframe
+        src="https://player.vimeo.com/video/1115222113?background=1&autoplay=1&muted=1&loop=1&dnt=1"
+        className="hero-video-frame"
+        allow="autoplay; fullscreen; picture-in-picture"
+        frameBorder="0"
+      />
+
+      {/* 黑色半透明遮罩，保证字可读 */}
+      <div className="absolute inset-0 bg-black/40" />
+    </div>
+  );
+}
